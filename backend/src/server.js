be9ec7df1d1c6ff69514c200b7158b1c6aa1993f
VARSHINI P <p.varshini258@gmail.com>
2026-09-16 20:27:05 +0530

    Connect backend to MongoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,11 @@
+require("dotenv").config();
+
 const app = require("./app");
+const connectDB = require("./config/db");
 
 const PORT = 5001;
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
