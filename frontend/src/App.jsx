afdf610d2b54b0bf6355cf683c49ce248078ecf5
VARSHINI P <p.varshini258@gmail.com>
2026-09-16 20:53:24 +0530

    Set up React frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Pathology Laboratory Management System</h1>
+      <p>Frontend is running successfully.</p>
+    </div>
+  );
+}
+
+export default App;
